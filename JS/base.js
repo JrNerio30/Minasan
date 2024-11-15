747f9a020a03346e3c901d6bffec9098e7e59904
Fernando Nerio Jr <fernandonerio90@gmail.com>
2024-11-15 13:30:29 -0700

    feat: add last 2 recipefunction from the recipe json file

--- JS/base.js
+++ JS/base.js
@@ -1,38 +1,188 @@
+/* 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    RECIPES NAVIGATION MENU
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+const recipesNavMenus = document.querySelectorAll("div.recipe-menus a");
+const recipeNav = [recipesMenu, recipeLinks];
+
+recipeNav.forEach((hoverables) => {
+  hoverables.addEventListener("mouseover", () => {
+    recipeLinks.classList.add("active");
+  
+    recipesNavMenus.forEach((links) => {
+      links.classList.add("active");
+    });
+  });
+})
+
+recipeNav.forEach((hoverables) => {
+  hoverables.addEventListener("mouseout", () => {
+    recipeLinks.classList.remove("active");
+  
+    recipesNavMenus.forEach((links) => {
+      links.classList.remove("active");
+    });
+  });
+})
+
+/* 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+      SPAGHETTI CARBONARA
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
 function spaghettiCarbonara() {
-  const spaghettiCarbonaraRecipe = recipes[0][0];
-  spaghettiCarbonaraImg.src = spaghettiCarbonaraRecipe.image;
+  const recipe = recipes[0][0];
+
+  const updateElement = (element, content) => {
+    if (element) element.innerHTML = content;
+  };
+
+  spaghettiCarbonaraImg.src = recipe.image;
   spaghettiCarbonaraImg.alt = "Spaghetti Carbonara on black bowl with a tomato on top.";
 
-  spaghettiCarbonaraName.innerHTML = spaghettiCarbonaraRecipe.name;
-  spaghettiCarbonaraDescription.innerHTML = spaghettiCarbonaraRecipe.description;
-  spaghettiCarbonaraDifficulty.innerHTML = `<span>Difficulty:</span> ${spaghettiCarbonaraRecipe.difficulty}`
-  spaghettiCarbonaraCuisine.innerHTML = `<span>Cuisine:</span> ${spaghettiCarbonaraRecipe.cuisine}`
+  updateElement(spaghettiCarbonaraName, recipe.name);
+  updateElement(spaghettiCarbonaraLikes, `<i class="fa-solid fa-heart"></i> ${recipe.likes}`);
+  updateElement(spaghettiCarbonaraDescription, recipe.description);
+  updateElement(spaghettiCarbonaraCuisine, `<span>Cuisine:</span> ${recipe.cuisine}`);
+  updateElement(spaghettiCarbonaraDifficulty, `<span>Difficulty:</span> ${recipe.difficulty}`);
+  updateElement(spaghettiCarbonaraServings, `<span>Servings:</span> ${recipe.servings}`)
+  updateElement(spaghettiCarbonaraPrep, `<span>Prep Time:</span> ${recipe.prepTime} minutes`);
+  updateElement(spaghettiCarbonaraCook, `<span>Cook Time:</span> ${recipe.cookTime} minutes`);
+  updateElement(spaghettiCarbonaraTotal, `<span>Total:</span> ${recipe.totalTime} minutes`);
 
-  spaghettiCarbonaraPrep.innerHTML = `<span>Prep Time:</span> ${spaghettiCarbonaraRecipe.prepTime} minutes`
-  spaghettiCarbonaraCook.innerHTML = `<span>Cook Time:</span> ${spaghettiCarbonaraRecipe.cookTime} minutes`
-  spaghettiCarbonaraTotal.innerHTML = `<span>Total:</span> ${spaghettiCarbonaraRecipe.totalTime} minutes`
+  const populateList = (parentElement, items, formatter) => {
+    parentElement.innerHTML = '';
+    items.forEach(item => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = formatter(item);
+      parentElement.appendChild(listItem);
+    });
+  };
 
-  for(i = 0; i < spaghettiCarbonaraRecipe.ingredients.length; i++) {
-    const listIngredients = document.createElement("li");
-    const recipeData = spaghettiCarbonaraRecipe.ingredients[i];
-    listIngredients.innerHTML = `${recipeData.amount} ${recipeData.unit} of ${recipeData.item}`;
-    recipeIngredients.appendChild(listIngredients);
-  }
+  populateList(spaghettiCarbonaraIngredients, recipe.ingredients, (ingredient) => 
+    `${ingredient.amount} ${ingredient.unit} of ${ingredient.item}`
+  );
 
-  for(i = 0; i < spaghettiCarbonaraRecipe.instructions.length; i++) {
-    const listIngredients = document.createElement("li");
-    const recipeData = spaghettiCarbonaraRecipe.instructions[i];
-    listIngredients.innerHTML = `<span>Step ${recipeData.step}:</span> ${recipeData.text}.`;
-    recipeInstructions.appendChild(listIngredients);
-  }
+  populateList(spaghettiCarbonaraInstructions, recipe.instructions, (instruction) => 
+    `<span>Step ${instruction.step}:</span> ${instruction.text}.`
+  );
 
-  const nutritionFacts = spaghettiCarbonaraRecipe.nutritionalInfo;
+  const nutrition = recipe.nutritionalInfo;
+  updateElement(spaghettiCarbonaraCalories, `Calories <span>${nutrition.calories}</span>`);
+  updateElement(spaghettiCarbonaraProtein, `Protein <span>${nutrition.protein}</span>`);
+  updateElement(spaghettiCarbonaraCarbs, `Carbohydrates <span>${nutrition.carbohydrates}</span>`);
+  updateElement(spaghettiCarbonaraFat, `Fat <span>${nutrition.fat}</span>`);
 
-  spaghettiCarbonaraCalories.innerHTML = `Calories <span>${nutritionFacts.calories}</span>`
-  spaghettiCarbonaraProtein.innerHTML = `Protein <span>${nutritionFacts.protein}</span>`
-  spaghettiCarbonaraCarbs.innerHTML = `Carbohydrates <span>${nutritionFacts.carbohydrates}</span>`
-  spaghettiCarbonaraFat.innerHTML = `Fat <span>${nutritionFacts.fat}</span>`
-
+  updateElement(spaghettiCarbonaraTags, `<span>Tags:</span> ${recipe.tags.join(', ')}`);
 }
-spaghettiCarbonara()
+
+spaghettiCarbonara();
+
+
+/* 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  ITALIAN SAUSAGE SPAGHETTI SQUASH
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+function iSSS() {
+  const recipe = recipes[0][1];
+
+  const updateElement = (element, content) => {
+    if (element) element.innerHTML = content;
+  };
+
+iSSSImg.src = recipe.image;
+iSSSImg.alt = "Spaghetti Carbonara on black bowl with a tomato on top.";
+
+  updateElement(iSSSName, recipe.name);
+  updateElement(iSSSLikes, `<i class="fa-solid fa-heart"></i> ${recipe.likes}`);
+  updateElement(iSSSDescription, recipe.description);
+  updateElement(iSSSCuisine, `<span>Cuisine:</span> ${recipe.cuisine}`);
+  updateElement(iSSSDifficulty, `<span>Difficulty:</span> ${recipe.difficulty}`);
+  updateElement(iSSSServings, `<span>Servings:</span> ${recipe.servings}`)
+  updateElement(iSSSPrep, `<span>Prep Time:</span> ${recipe.prepTime} minutes`);
+  updateElement(iSSSCook, `<span>Cook Time:</span> ${recipe.cookTime} minutes`);
+  updateElement(iSSSTotal, `<span>Total:</span> ${recipe.totalTime} minutes`);
+
+  const populateList = (parentElement, items, formatter) => {
+    parentElement.innerHTML = '';
+    items.forEach(item => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = formatter(item);
+      parentElement.appendChild(listItem);
+    });
+  };
+
+  populateList(iSSSIngredients, recipe.ingredients, (ingredient) => 
+    `${ingredient.amount} ${ingredient.unit} of ${ingredient.item}`
+  );
+
+  populateList(iSSSInstructions, recipe.instructions, (instruction) => 
+    `<span>Step ${instruction.step}:</span> ${instruction.text}.`
+  );
+
+  const nutrition = recipe.nutritionalInfo;
+  updateElement(iSSSCalories, `Calories <span>${nutrition.calories}</span>`);
+  updateElement(iSSSProtein, `Protein <span>${nutrition.protein}</span>`);
+  updateElement(iSSSCarbs, `Carbohydrates <span>${nutrition.carbohydrates}</span>`);
+  updateElement(iSSSFat, `Fat <span>${nutrition.fat}</span>`);
+
+  updateElement(iSSSTags, `<span>Tags:</span> ${recipe.tags.join(', ')}`);
+}
+
+iSSS();
+
+/* 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+      VEGAN MUSHROOM RAGOUT
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+function veganMushroomRagout() {
+  const recipe = recipes[0][2];
+
+  const updateElement = (element, content) => {
+    if (element) element.innerHTML = content;
+  };
+
+  veganMushroomRagoutImg.src = recipe.image;
+  veganMushroomRagoutImg.alt = "Spaghetti Carbonara on black bowl with a tomato on top.";
+
+  updateElement(veganMushroomRagoutName, recipe.name);
+  updateElement(veganMushroomRagoutLikes, `<i class="fa-solid fa-heart"></i> ${recipe.likes}`);
+  updateElement(veganMushroomRagoutDescription, recipe.description);
+  updateElement(veganMushroomRagoutCuisine, `<span>Cuisine:</span> ${recipe.cuisine}`);
+  updateElement(veganMushroomRagoutDifficulty, `<span>Difficulty:</span> ${recipe.difficulty}`);
+  updateElement(veganMushroomRagoutServings, `<span>Servings:</span> ${recipe.servings}`)
+  updateElement(veganMushroomRagoutPrep, `<span>Prep Time:</span> ${recipe.prepTime} minutes`);
+  updateElement(veganMushroomRagoutCook, `<span>Cook Time:</span> ${recipe.cookTime} minutes`);
+  updateElement(veganMushroomRagoutTotal, `<span>Total:</span> ${recipe.totalTime} minutes`);
+
+  const populateList = (parentElement, items, formatter) => {
+    parentElement.innerHTML = '';
+    items.forEach(item => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = formatter(item);
+      parentElement.appendChild(listItem);
+    });
+  };
+
+  populateList(veganMushroomRagoutIngredients, recipe.ingredients, (ingredient) => 
+    `${ingredient.amount} ${ingredient.unit} of ${ingredient.item}`
+  );
+
+  populateList(veganMushroomRagoutInstructions, recipe.instructions, (instruction) => 
+    `<span>Step ${instruction.step}:</span> ${instruction.text}.`
+  );
+
+  const nutrition = recipe.nutritionalInfo;
+  updateElement(veganMushroomRagoutCalories, `Calories <span>${nutrition.calories}</span>`);
+  updateElement(veganMushroomRagoutProtein, `Protein <span>${nutrition.protein}</span>`);
+  updateElement(veganMushroomRagoutCarbs, `Carbohydrates <span>${nutrition.carbohydrates}</span>`);
+  updateElement(veganMushroomRagoutFat, `Fat <span>${nutrition.fat}</span>`);
+
+  updateElement(veganMushroomRagoutTags, `<span>Tags:</span> ${recipe.tags.join(', ')}`);
+}
+
+veganMushroomRagout();
 
